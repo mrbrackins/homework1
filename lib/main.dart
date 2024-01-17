ca5cbfa0c145de58fefc89fba7ae1d4db7e923ef
Tony Brackins <tonyb@DWCA.LOCAL>
2024-01-17 14:51:47 -0800

    added lists dynamically

--- lib/main.dart
+++ lib/main.dart
@@ -19,34 +19,41 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var names = {
+  // Key:    Value
+  'Teenager 1': '15',
+  'Teenager 2': '16',
+  'Teenager 3': '17 ',
+  'Teenager 4': '15',
+  'Teenager 5': '16',
+  'Teenager 6': '17 ',
+  'Teenager 7': '15',
+  'Teenager 8': '16',
+  'Teenager 9': '17 ',
+  'Teenager 10': '15',
+  'Teenager 11': '16',
+  'Teenager 12': '17 ',
+  'Teenager 13': '15',
+  'Teenager 14': '16',
+  'Teenager 15': '17 '
+  
+};
+      List<String> litems = ["1","2","Third","4"];
     return Scaffold(
         appBar: AppBar(
           title: Text("Page 1"),
         ),
         body: Column(children: <Widget>[
           Expanded(
-              child: ListView(
-            padding: const EdgeInsets.all(8),
-            children: <Widget>[
-              Text("Name1, 15"),
-              Text("Name2, 15"),
-              Text("Name3, 15"),
-              Text("Name4, 15"),
-              Text("Name5, 15"),
-              Text("Name6, 15"),
-              Text("Name7, 15"),
-              Text("Name8, 15"),
-              Text("Name9, 15"),
-              Text("Name10, 15"),
-              Text("Name11, 15"),
-              Text("Name12, 15"),
-              Text("Name13, 15"),
-              Text("Name14, 15"),
-              Text("Name15, 15"),
-            ],
-          )),
-          Center(
-            child: ElevatedButton(
+              child: ListView.builder(
+    itemCount: names.length,
+    itemBuilder: (BuildContext ctxt, int index) {
+       String key = names.keys.elementAt(index);
+     return ListTile(
+      title: new Text("$key"),
+      subtitle: new Text("${names[key]}"),
+     );
+    }  ),), ElevatedButton(
               child: const Text(
                 'Navigate to a new screen >>',
                 style: TextStyle(fontSize: 24.0),
@@ -56,7 +63,9 @@ class Home extends StatelessWidget {
                     .push(MaterialPageRoute(builder: (context) => Page2()));
               },
             ),
-          ),
-        ]));
+       
+        ], 
+        )
+        );
   }
 }
--- lib/page2.dart
+++ lib/page2.dart
@@ -1,53 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:homework1/main.dart';
 
 class Page2 extends StatelessWidget {
   const Page2({super.key});
 
   @override
   Widget build(BuildContext context) {
+     List<String> countries = ["Brazil", "Canada","China","France", "Germany", "India", "Indonesia", "Itealy", "Japan", "Korea"];
     return Scaffold(
         appBar: AppBar(
           title: Text("Page 2"),
         ),
         body: Column(children: <Widget>[
           Expanded(
-              child: ListView(
-            padding: const EdgeInsets.all(8),
-            children: <Widget>[
-              Text("Brazil"),
-              Text("Canada"),
-              Text("China"),
-              Text("France"),
-              Text("Germany"),
-              Text("India"),
-              Text("Indonesia"),
-              Text("Italy"),
-              Text("Japan"),
-              Text("Republic of Korea"),
-              Text("Mexico"),
-              Text("Russia"),
-              Text("Saudi Arabia"),
-              Text("South Africa"),
-              Text("Türkiye"),
-              Text("United Kingdom"),
-              Text("United States"),
-              Text("Ghana"),
-              Text("Nigeria"),
-              Text("Australia"),
-            ],
-          )),
-          Center(
-            child: ElevatedButton(
+              child: ListView.builder(
+    itemCount: countries.length,
+    itemBuilder: (BuildContext ctxt, int index) {
+      
+     return ListTile(
+      title: new Text(countries[index]),
+    
+     );
+    }  
+    ),
+      
+        ), 
+         ElevatedButton(
               child: const Text(
-                'Navigate to a new screen >>',
+                'Navigate back>>',
                 style: TextStyle(fontSize: 24.0),
               ),
               onPressed: () {
                 Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (context) => Page2()));
+                    .push(MaterialPageRoute(builder: (context) => MyApp()));
               },
             ),
-          ),
-        ]));
+        ]
+         )   
+             );
   }
-}
+  }
